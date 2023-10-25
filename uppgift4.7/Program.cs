88bdd9379ad80b4f91562db5a125c90ceb63ff76
Bard1en <greger.grym@gmail.com>
2023-10-25 07:51:16 +0200

    Add project files.

---
+++ uppgift4.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace upg4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine("Hur många mellanslag ifrån kanten ska det vara?");
+            int spaces = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                if (spaces > 0)
+                {
+                    for (int j = 1; j <= spaces; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
